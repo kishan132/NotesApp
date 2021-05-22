490fbf622644ac2a1d77c8d3f7fc142b01c5b814
Shashi Kant Yadav <shashikant231217@gmail.com>
2021-05-22 16:00:30 +0530

    updated

--- messages/index.js
+++ messages/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const verifyServiceSid = process.env.TWILIO_VERIFY_SERVICE_SID;
